8b1b624b19a0d41c4a3b809e2a74aa2afa1e36eb
banica <matei.cioangher@gmail.com>
2025-02-15 13:10:02 +0200

    Create exercitiu1.c

---
+++ Exercitii/exercitiu1.c
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cstring>
+using namespacestd;
+int main(){
+char a[21];
+char b[21];
+cin.get(b,21)
+if(strlen (b)>=3){
+strcpy(a, b[strchr (b)-3])}else{strcpy(a,"nedeterminat")}
+} cout<<a;
