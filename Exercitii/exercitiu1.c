2848e819ceb8503782decaccc9b6a4b830fcbd56
banica <danpietrareanu528@gmail.com>
2025-02-15 13:16:30 +0200

    Update exercitiu1.c

--- Exercitii/exercitiu1.c
+++ Exercitii/exercitiu1.c
@@ -1,12 +1,23 @@
 // Exercitiu 1 din culegerea de admitere la UPB
 
-#include<iostream>
-#include<cstring>
-using namespacestd;
-int main(){
-char a[21];
-char b[21];
-cin.get(b,21)
-if(strlen (b)>=3){
-strcpy(a, b[strchr (b)-3])}else{strcpy(a,"nedeterminat")}
-} cout<<a;
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    char a[21]; //definire sir a
+    char b[21]; //definire sir b
+
+    cin.get(b, 21);
+
+    if (strlen(b) >= 3) {
+        strcpy(a, strchr(b, '\0') - 3); // adauga comentarii la cod ca sa il intelegi
+    } else {
+        strcpy(a, "nedeterminat");
+    }
+
+    cout << a;
+
+    return 0;
+}
